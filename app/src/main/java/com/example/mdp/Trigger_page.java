6401e40515645d70e4cc6cfa0a4398b87ef2d14e
Pasindu Theekshana <pasindutheekshana21@gmail.com>
2026-03-12 10:33:13 +0530

    Remove image icon activity_trigger_page.xml UI

--- app/src/main/java/com/example/mdp/Trigger_page.java
+++ app/src/main/java/com/example/mdp/Trigger_page.java
@@ -1,41 +1,24 @@
 package com.example.mdp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.card.MaterialCardView;
 
 public class Trigger_page extends AppCompatActivity {
-
-    private MaterialCardView btnSosCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_trigger_page);
-        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // Find the SOS button by ID
-        btnSosCall = findViewById(R.id.btnSosCall);
-
-        // Set click listener to open the Confirmation Dialog
-        btnSosCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Trigger_page.this, Confirmation_Dialog.class);
-                startActivity(intent);
-            }
         });
     }
 }
